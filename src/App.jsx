858c0a77f5f6beccdcbdddb93fe9d4b85737c6bb
Jefferson Abreu <jehffersson@hotmail.com>
2023-07-04 20:39:48 -0300

    fix: app

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,40 @@ import Leiaute from './pages/leiaute';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import NotFound from './pages/NotFound';
-// Alunos
-import ListagemAluno from './pages/alunos/Listagem';
-import CadastroAluno from './pages/alunos/Cadastro';
-import AlteracaoAluno from './pages/alunos/Alteracao';
-import ExclusaoAluno from './pages/ufs/Exclusao';
-// UF
-import ListagemUF from './pages/ufs/Listagem';
-import CadastroUF from './pages/ufs/Cadastro';
-import AlteracaoUF from './pages/ufs/Alteracao';
-import ExclusaoUF from './pages/ufs/Exclusao';
+
 // Instrutor
 import ListagemInstrutor from './pages/instrutores/Listagem';
 import CadastroInstrutor from './pages/instrutores/Cadastro';
 import AlteracaoInstrutor from './pages/instrutores/Alteracao';
 import ExclusaoInstrutor from './pages/instrutores/Exclusao';
+// Alunos
+import ListagemAluno from './pages/alunos/Listagem';
+import CadastroAluno from './pages/alunos/Cadastro';
+import AlteracaoAluno from './pages/alunos/Alteracao';
+import ExclusaoAluno from './pages/alunos/Exclusao';
+// UF
+import ListagemUF from './pages/ufs/Listagem';
+import CadastroUF from './pages/ufs/Cadastro';
+import AlteracaoUF from './pages/ufs/Alteracao';
+import ExclusaoUF from './pages/ufs/Exclusao';
+// City
+import ListagemCity from './pages/citys/Listagem';
+import CadastroCity from './pages/citys/Cadastro';
+import AlteracaoCity from './pages/citys/Alteracao';
+import ExclusaoCity from './pages/citys/Exclusao';
+
+// District
+// Breed
+// Dog
+// DogSize
+// Employee
+// Guardianship
+// Occurrence
+// Tutor
+// TypeOfOccurrence
+// Vaccination
+// Vaccine
+// Veterinarian
 
 
 const App = () => {
@@ -39,17 +58,23 @@ const App = () => {
                 <Route path="alterar/:id" element={<AlteracaoAluno />} />
                 <Route path="excluir/:id" element={<ExclusaoAluno />} />
               </Route>
+              <Route path="instrutores">
+                <Route index element={<ListagemInstrutor />} />
+                <Route path="cadastrar" element={<CadastroInstrutor />} />
+                <Route path="alterar/:id" element={<AlteracaoInstrutor />} />
+                <Route path="excluir/:id" element={<ExclusaoInstrutor />} />
+              </Route>
               <Route path="ufs">
                 <Route index element={<ListagemUF />} />
                 <Route path="cadastrar" element={<CadastroUF />} />
                 <Route path="alterar/:id" element={<AlteracaoUF />} />
                 <Route path="excluir/:id" element={<ExclusaoUF />} />
               </Route>
-              <Route path="instrutores">
-                <Route index element={<ListagemInstrutor />} />
-                <Route path="cadastrar" element={<CadastroInstrutor />} />
-                <Route path="alterar/:id" element={<AlteracaoInstrutor />} />
-                <Route path="excluir/:id" element={<ExclusaoInstrutor />} />
+              <Route path="citys">
+                <Route index element={<ListagemCity />} />
+                <Route path="cadastrar" element={<CadastroCity />} />
+                <Route path="alterar/:id" element={<AlteracaoCity />} />
+                <Route path="excluir/:id" element={<ExclusaoCity />} />
               </Route>
               <Route path="*" element={<NotFound />} />
             </Route>
